c08391695beb08953ad28cf5fc717e791037cab6
sebal27 <sbalasu27@gmail.com>
2023-03-23 00:35:18 +0100

    update

--- api/terraform/cloud.run.tf
+++ api/terraform/cloud.run.tf
@@ -32,7 +32,7 @@ resource "google_cloud_run_service" "this" {
           }
         }
         env {
-          name  = "DBPASS"
+          name = "DBPASS"
           value_from {
             secret_key_ref {
               key  = "latest"
